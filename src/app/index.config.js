65e12edca022ed91d36b36705addd89113301669
Vitor Fernandes <vmlf01@gmail.com>
2016-01-10 22:11:36 +0000

    feat: add login form directive

--- src/app/index.config.js
+++ src/app/index.config.js
@@ -1,21 +1,29 @@
-(function() {
-  'use strict';
+(function () {
+    'use strict';
 
-  angular
-    .module('bomVizinhoWeb')
-    .config(config);
+    angular
+        .module('bomVizinhoWeb')
+        .config(config)
+        .config(configSatellizer);
 
-  /** @ngInject */
-  function config($logProvider, toastrConfig) {
-    // Enable log
-    $logProvider.debugEnabled(true);
+    /** @ngInject */
+    function config($logProvider, toastrConfig) {
+        // Enable log
+        $logProvider.debugEnabled(true);
 
-    // Set options third-party lib
-    toastrConfig.allowHtml = true;
-    toastrConfig.timeOut = 3000;
-    toastrConfig.positionClass = 'toast-top-right';
-    toastrConfig.preventDuplicates = true;
-    toastrConfig.progressBar = true;
-  }
+        // Set options third-party lib
+        toastrConfig.allowHtml = true;
+        toastrConfig.timeOut = 3000;
+        toastrConfig.positionClass = 'toast-top-right';
+        toastrConfig.preventDuplicates = true;
+        toastrConfig.progressBar = true;
+    }
+
+    /** @ngInject */
+    function configSatellizer($authProvider) {
+        $authProvider.facebook({
+            clientId: '1691283711115807'
+        });
+    }
 
 })();
