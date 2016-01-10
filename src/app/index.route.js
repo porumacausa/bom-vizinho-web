556f6fa5cb77851972343ff5bd1862c586cc20d3
Vitor Fernandes <vitor.fernandes@gotvmediasoftware.com>
2016-01-10 11:07:11 +0000

    feat: add about page

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -19,6 +19,17 @@
         templateUrl: 'app/home/home.html',
         controller: 'HomeController',
         controllerAs: 'vm'
+      })
+      .state('site.about', {
+        url: '/about',
+        templateUrl: 'app/about/about.html',
+        controller: 'AboutController',
+        controllerAs: 'vm',
+        resolve: {
+          technologies: function (webDevTec) {
+            return webDevTec.getTec();
+          }
+        }
       });
 
     $urlRouterProvider.otherwise(function($injector) {
